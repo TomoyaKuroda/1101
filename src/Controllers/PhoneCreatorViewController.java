974a39687a9580f78aeb02c3ad978033f0ba7efa
JaretWright <hkkA223>
2018-09-21 10:09:57 -0400

    Updated with drivers

---
+++ src/Controllers/PhoneCreatorViewController.java
@@ -0,0 +1,60 @@
+package Controllers;
+
+import Models.DBConnect;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class PhoneCreatorViewController implements Initializable {
+    @FXML
+    private ChoiceBox<String> makeChoiceBox;
+
+    @FXML
+    private TextField modelTextField;
+
+    @FXML
+    private ChoiceBox<String> osChoiceBox;
+
+    @FXML
+    private TextField screenSizeTextField;
+
+    @FXML
+    private Slider slider;
+
+    @FXML
+    private Label memorySlider;
+
+    @FXML
+    private TextField frontCameraTextField;
+
+    @FXML
+    private TextField rearCameraTextField;
+
+    @FXML
+    private TextField priceTextField;
+
+    @FXML
+    private ImageView imageView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        //Preload the ChoiceBox with valid manufacturers
+        try {
+            makeChoiceBox.getItems().addAll(DBConnect.getPhoneManufacturers());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
