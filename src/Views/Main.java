05a9931e93e8ad6578a2d8cb4032d86dfffe771f
JaretWright <jaret.wright@georgiancollege.ca>
2018-10-05 11:13:02 -0400

    Added TableView layout

--- src/Views/Main.java
+++ src/Views/Main.java
@@ -1,10 +1,14 @@
 package Views;
 
+import Models.DBConnect;
+import Models.MobilePhone;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -15,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("PhoneCreatorView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TableOfPhonesView.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Create Mobile Phone");
